d5d59c6d2caac1449eb7019b2aa40541e5793533
snpatel17 <35784647+snpatel17@users.noreply.github.com>
2021-07-21 01:06:08 -0400

    error handling

--- BookStoreAPI/Controllers/BooksController.cs
+++ BookStoreAPI/Controllers/BooksController.cs
@@ -2,6 +2,7 @@
 using BookStoreAPI.Repository;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Threading.Tasks;
 
 namespace BookStoreAPI.Controllers
@@ -19,45 +20,87 @@ namespace BookStoreAPI.Controllers
         [HttpGet("")]
         public async Task<IActionResult> GetAllBooks()
         {
-            var books = await _bookRepository.GetAllBooksAsync();
-            return Ok(books);
+            try
+            {
+                var books = await _bookRepository.GetAllBooksAsync();
+                return Ok(books);
+            }
+            catch (Exception err)
+            {
+                return StatusCode(500, "Internal Server error");
+            }
         }
 
         [HttpGet("{bookId}")]
         public async Task<IActionResult> GetBookById([FromRoute] string bookId)
         {
-            var book = await _bookRepository.GetBookByIdAsync(bookId);
-            if (book == null)
+            try
             {
-                return NotFound();
+                var book = await _bookRepository.GetBookByIdAsync(bookId);
+                if (book == null)
+                {
+                    return NotFound();
+                }
+                return Ok(book);
             }
-            return Ok(book);
+            catch (Exception err)
+            {
+                return StatusCode(500, "Internal Server error");
+            }
         }
 
         [HttpPost("")]
         public async Task<IActionResult> AddNewBook([FromBody]BooksModel bookModel)
         {
-            var bookId = await _bookRepository.AddBookAsync(bookModel);
-            return CreatedAtAction(nameof(GetBookById), new {bookId = bookId, controller= "books" }, bookId);
+            try
+            {
+                var bookId = await _bookRepository.AddBookAsync(bookModel);
+                return CreatedAtAction(nameof(GetBookById), new { bookId = bookId, controller = "books" }, bookId);
+            }
+            catch (Exception err)
+            {
+                return StatusCode(500, "Internal Server error");  
+            }
         }
 
         [HttpPut("{bookId}")]
         public async Task<IActionResult> UpdateBook([FromBody] BooksModel bookModel, [FromRoute] string bookId)
         {
-            await _bookRepository.UpdateBookAsync(bookId,bookModel);
-            return Ok();
+            try
+            {
+                await _bookRepository.UpdateBookAsync(bookId, bookModel);
+                return Ok();
+            }
+            catch (Exception err)
+            {
+                return StatusCode(500, "Internal Server error");
+            }
         }
         [HttpPatch("{bookId}")]
         public async Task<IActionResult> UpdateBookPatch([FromBody] JsonPatchDocument bookModel, [FromRoute] string bookId)
         {
-            await _bookRepository.UpdateBookAsync(bookId, bookModel);
-            return Ok();
+            try
+            {
+                await _bookRepository.UpdateBookAsync(bookId, bookModel);
+                return Ok();
+            }
+            catch (Exception err)
+            {
+                return StatusCode(500, "Internal Server error");
+            }
         }
         [HttpDelete("{bookId}")]
         public async Task<IActionResult> DeleteBook([FromRoute] string bookId)
         {
-            await _bookRepository.DeleteBookAsync(bookId);
-            return Ok();
+            try
+            {
+                await _bookRepository.DeleteBookAsync(bookId);
+                return Ok();
+            }
+            catch (Exception err)
+            {
+                return StatusCode(500, "Internal Server error");
+            }
         }
     }
 }
